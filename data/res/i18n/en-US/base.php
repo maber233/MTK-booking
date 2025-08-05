8564b294caf1b5fa002645218c66bb791646b4bc
Martin Berg <martin.berg1@ingka.ikea.com>
2025-08-05 17:10:00 +0200

    Restore English base.php translation file
    - File was accidentally emptied during manual edits
    - Restored all English base translations (months, days, navigation, etc.)
    - This fixes potential translation issues when switching to English

---
+++ data/res/i18n/en-US/base.php
@@ -0,0 +1,77 @@
+<?php
+
+return [
+
+    'The appointment' => 'The appointment',
+    'for' => 'for',
+    'on' => 'on',
+    'at' => 'at',
+    'with' => 'with',
+
+    'Calendar' => 'Calendar',
+    'Next' => 'Next',
+    'Back' => 'Back',
+
+    'of' => 'of',
+    'and' => 'and',
+    'or' => 'or',
+    'The' => 'The',
+    'A' => 'A',
+    'No' => 'No',
+    'Yes' => 'Yes',
+    'Cancel' => 'Cancel',
+    'Apply' => 'Apply',
+    'OK' => 'OK',
+
+    'January' => 'January',
+    'February' => 'February',
+    'March' => 'March',
+    'April' => 'April',
+    'May' => 'May',
+    'June' => 'June',
+    'July' => 'July',
+    'August' => 'August',
+    'September' => 'September',
+    'October' => 'October',
+    'November' => 'November',
+    'December' => 'December',
+
+    'Monday' => 'Monday',
+    'Tuesday' => 'Tuesday',
+    'Wednesday' => 'Wednesday',
+    'Thursday' => 'Thursday',
+    'Friday' => 'Friday',
+    'Saturday' => 'Saturday',
+    'Sunday' => 'Sunday',
+
+    'Mon' => 'Mon',
+    'Tue' => 'Tue',
+    'Wed' => 'Wed',
+    'Thu' => 'Thu',
+    'Fri' => 'Fri',
+    'Sat' => 'Sat',
+    'Sun' => 'Sun',
+
+    'Home' => 'Home',
+    'Info' => 'Info',
+    'Do your like our service?' => 'Do you like our service?',
+    'Contact us:' => 'Contact us:',
+
+    'Administration' => 'Administration',
+
+    'Not found' => 'Not found',
+    'Unsupported operation' => 'Unsupported operation',
+    'An error occurred' => 'An error occurred',
+    'The requested controller was unable to dispatch the request.' => 'The requested controller was unable to dispatch the request.',
+    'Controller' => 'Controller',
+    'resolves to %s' => 'resolves to %s',
+    'Exception' => 'Exception',
+    'Stack trace' => 'Stack trace',
+    'Message' => 'Message',
+    'File' => 'File',
+    'Line' => 'Line',
+
+    'Previous exceptions' => 'Previous exceptions',
+    'No Exception available' => 'No Exception available',
+
+];
