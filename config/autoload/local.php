ef0683f58ce027f09590169f365f6ea3ccc6563d
Martin Berg <martin.berg1@ingka.ikea.com>
2025-08-24 19:10:06 +0200

    Fix database configuration format for Zend adapter compatibility

---
+++ config/autoload/local.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Local Configuration Override for Cloud Run
+ *
+ * This file provides database configuration using environment variables
+ * for deployment on Google Cloud Run with Cloud SQL.
+ */
+
+return [
+    'db' => [
+        'driver'         => 'Pdo_Mysql',
+        'dsn'            => 'mysql:unix_socket=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8mb4',
+        'username'       => $_ENV['DB_USER'],
+        'password'       => $_ENV['DB_PASSWORD'],
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ],
+    ],
+    'mail' => [
+        'type' => 'sendmail',
+        'address' => 'info@mtk-booking.com',
+    ],
+];
